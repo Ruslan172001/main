2306777cb2abc03bc1b92e5341d7df1650ee6fd8
Ruslan <russerg225@mail.ru>
2021-10-08 14:04:26 +0600

    while do

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const personalMovieDB = {
     private: false
 };
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) do{
     const a = prompt('Один из последних просмотренных фильмов?'),
           b = prompt('На сколько оцените его?');   
     if(a != '' && b != '' && a != null && b != null && a.length < 50) {
@@ -21,14 +21,15 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
-if (personalMovieDB.count < 10) {
+while (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-} else if (personalMovieDB.count > 30) {
-        console.log('Вы киноман');
-} else {
-        console.log('Произошла ошибка');
-}
+    if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+    } else {
+    console.log('Произошла ошибка');
+    }
+    } 
 
 console.log(personalMovieDB);
